29ffbf035577ff63ec160c1aa948c457e9252ffc
Harsh Shandilya <msfjarvis@gmail.com>
2017-05-10 14:46:12 +0530

    healthd: Add missing call to CLEAR_VARS

--- healthd/Android.mk
+++ healthd/Android.mk
@@ -51,7 +51,7 @@ LOCAL_STATIC_LIBRARIES := \
 
 include $(BUILD_STATIC_LIBRARY)
 
-
+include $(CLEAR_VARS)
 LOCAL_SRC_FILES := healthd_board_msm.cpp
 LOCAL_MODULE := libhealthd.qcom
 LOCAL_CFLAGS := -Werror
